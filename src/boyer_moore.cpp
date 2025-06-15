71ee74d3a3566d1e74c8c1f912f7ccdb1425850d
4lehh <camposjavier143@gmail.com>
2025-06-15 01:06:28 -0400

    feat: Algoritmo de Boyer-Moore

--- src/boyer_moore.cpp
+++ src/boyer_moore.cpp
@@ -1,6 +1,40 @@
 #include "../include/definiciones.hpp"
 #include "../include/boyer_moore.hpp"
+#include <string>
+#include <iostream>
 
 bool BoyerMoore::buscar(const std::string& nombre_archivo, const std::string& patron) {
-    // Implementación del algoritmo Boyer-Moore
+    int tamanio_patron = patron.length(), tamanio_nombre_archivo = nombre_archivo.length();
+    
+    // Si no tiene largo el patron o el texto
+    if(!tamanio_patron || !tamanio_nombre_archivo) return false;
+    
+    int desplazamiento = 0;
+    
+    while(desplazamiento <= tamanio_nombre_archivo - tamanio_patron){
+        // Ultimo elemento del patron
+        int i = tamanio_patron - 1;
+        
+        // Si hay coincidencias, avanza al revés
+        while(i >= 0 && patron[i] == nombre_archivo[desplazamiento + i]){
+            i--;
+        }
+
+        // Ahora comparamos si i llego a 0, entonces encontró el patron
+        if(i <= 0) return true;
+        
+        else {
+            // Pillar similitudes menores
+            int aux = tamanio_patron - 1;
+            
+            // Se compara desde la similitud, ahi comparamos los elementos
+            while(aux >= 0 && nombre_archivo[desplazamiento + i] != patron[aux]) aux--;
+            
+            // Tomar los cambios
+            if(aux <= 0) desplazamiento += tamanio_patron;
+            // Avanzamos un poco hasta que encage con el patron
+            else desplazamiento += aux;
+        }
+    }
+    return false;
 }
--- src/suffix_arrays.cpp
+++ src/suffix_arrays.cpp
@@ -1,6 +1,18 @@
 #include "../include/definiciones.hpp"
 #include "../include/suffix_arrays.hpp"
+#include <iostream>
+#include <string>
 
 bool SuffixArrays::buscar(const std::string& nombre_archivo, const std::string& patron) {
     // Implementación del algoritmo Suffix Arrays
+    
+}
+
+int main(){
+    std::string texto = "Hola quisiera comer helado de piña";
+    std::string patron = "comer";
+
+    std::string mensaje = SuffixArrays::buscar(texto,patron) ? "Exito" : "Fracaso";
+    std::cout << mensaje << std::endl;
+
 }
