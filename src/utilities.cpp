1585564ae483a0986061f49762378e6480f874e5
AntoCreed777 <abenavides@desem.cl>
2025-06-22 21:02:36 -0400

    chore: Eliminacion de libreria inesesaria (linenoise)

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -4,60 +4,18 @@
 
 #include <cstring>
 #include <fstream>
-
-#define ARCHIVO_HISTORY_FILES "history_files.txt"
-#define ARCHIVO_HISTORY_PATRON "history_patron.txt"
-
-void completionArchivo(const char *buf, linenoiseCompletions *lc) {
-    std::string prefix(buf);
-    for (const auto& entry : fs::directory_iterator(fs::current_path())) {
-        if (entry.is_regular_file()) {
-            std::string filename = entry.path().filename().string();
-            if (filename.find(prefix) == 0) {
-                linenoiseAddCompletion(lc, filename.c_str());
-            }
-        }
-    }
-}
-
-void completionPatron(const char *buf, linenoiseCompletions *lc) {
-    // Sugerencias de patrones comunes
-    if (strlen(buf) == 0) {
-        linenoiseAddCompletion(lc, "Hola");
-        linenoiseAddCompletion(lc, "Mundo");
-    }
-}
-
-char* hintsArchivo(const char* buf, int* color, int* bold) {
-    *color = 36; *bold = 0;
-    return nullptr;
-}
-
-char* hintsPatron(const char* buf, int* color, int* bold) {
-    *color = 35; *bold = 0;
-    return nullptr;
-}
+#include <sstream>
 
 fs::path solicitarArchivo() {
-    // --- Inicialización de callbacks y carga de historial para linenoise (archivos) ---
-    linenoiseHistoryFree();     // Limpiar historial en memoria para evitar mezclar los historiales
-    linenoiseHistoryLoad(ARCHIVO_HISTORY_FILES);        // Cargar historial de archivos
-    linenoiseSetCompletionCallback(completionArchivo);  // Autocompletado de archivos
-    linenoiseSetHintsCallback(hintsArchivo);            // Sugerencias de color para archivos
-    // -------------------------------------------------------------------------------
-
     fs::path nombre_archivo;
 
     while (true) {
-        char* input = linenoise(CIAN "Ingrese el nombre del archivo (o 'exit' para salir): " RESET_COLOR);
-        if (input == NULL) break;
-
-        std::string entrada(input);
-        linenoiseFree(input);
+        std::string entrada;
+        std::cout << CIAN "Ingrese el nombre del archivo (o 'exit' para salir): " RESET_COLOR;
+        std::cin >> entrada;
 
         if (entrada == "exit") {
             std::cout << BLANCO "Saliendo del programa...\n" RESET_COLOR;
-            linenoiseHistorySave(ARCHIVO_HISTORY_FILES);
             exit(0);
         }
 
@@ -78,55 +36,31 @@ fs::path solicitarArchivo() {
         if (!fs::exists(nombre_archivo)) {
             std::cerr << ROJO "Error: El archivo no existe. Intente nuevamente." RESET_COLOR << '\n';
         }
-        else {
-            linenoiseHistoryAdd(entrada.c_str());
-            break;
-        }
+        else break;
     }
 
     nombre_archivo = fs::absolute(nombre_archivo); // Convertir a ruta absoluta
-
-    /* Save the history on disk. */
-    if (linenoiseHistorySave(ARCHIVO_HISTORY_FILES) == -1) {
-        std::cerr << ROJO "No se pudo guardar el historial de archivos" RESET_COLOR << std::endl;
-    }
 
     return nombre_archivo;
 }
 
 void solicitarPatron(std::string& patron) {
-    // --- Inicialización de callbacks y carga de historial para linenoise (archivos) ---
-    linenoiseHistoryFree();     // Limpiar historial en memoria para evitar mezclar los historiales
-    linenoiseHistoryLoad(ARCHIVO_HISTORY_PATRON);        // Cargar historial de archivos
-    linenoiseSetCompletionCallback(completionArchivo);  // Autocompletado de archivos
-    linenoiseSetHintsCallback(hintsArchivo);            // Sugerencias de color para patrones
-    // -------------------------------------------------------------------------------
-    
     while (true) {
-        char* input = linenoise(CIAN "Ingrese el patrón a buscar (o 'exit' para salir): " RESET_COLOR);
-        if (!input) break;
-
-        std::string entrada(input);
-        linenoiseFree(input);
+        std::string entrada;
+        std::cout << CIAN "Ingrese el patrón a buscar (o 'exit' para salir): " RESET_COLOR;
+        std::cin >> entrada;
 
         if (entrada == "exit") {
             std::cout << BLANCO "Saliendo del programa...\n" RESET_COLOR;
-            linenoiseHistorySave(ARCHIVO_HISTORY_PATRON);
             exit(0);
         }
 
         if (!entrada.empty()) {
             patron = entrada;
-            linenoiseHistoryAdd(entrada.c_str());
             break;
         } else {
             std::cerr << ROJO "Error: El patrón no puede estar vacío. Intente nuevamente." RESET_COLOR << '\n';
         }
-    }
-
-    /* Save the history on disk. */
-    if (linenoiseHistorySave(ARCHIVO_HISTORY_PATRON) == -1) {
-        throw std::runtime_error("No se pudo guardar el historial de patrones");
     }
 }
 
